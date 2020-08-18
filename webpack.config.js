4d087578b2716a08e7c2cb54ef7a95b8f0c90ec6
Aleksei Smolyaninov <salexxx@ngs.ru>
2020-08-18 12:54:35 +0700

    настроил инфраструктуру добавил файловую структуру

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
+const cssnano = require('cssnano');
 
 module.exports = {
-  entry: { main: './src/index.js' },
+  entry: {
+    main: './src/js/index.js',
+    articles: './src/js/articles/index.js',
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[chunkhash].js',
+    filename: './js/[name].[chunkhash].js',
   },
   module: {
     rules: [{
@@ -19,16 +24,43 @@ module.exports = {
       test: /\.css$/,
       use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
     },
+    {
+      test: /\.(png|jpe?g|svg|gif|ico)$/,
+      use: [
+        { loader: 'file-loader?name=./images/[name].[ext]' },
+        { loader: 'image-webpack-loader' },
+      ],
+      exclude: /node_modules/,
+    },
+    {
+      test: /\.(eot|ttf|woff|woff2)$/,
+      use: { loader: 'file-loader?name=./vendor/[name].[ext]' },
+    },
     ],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'style.[contenthash].css',
+      filename: './style/style.[contenthash].css',
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/,
+      cssProcessor: cssnano,
+      cssProcessorPluginOptions: {
+        preset: ['default'],
+      },
+      canPrint: true,
     }),
     new HtmlWebpackPlugin({
       inject: false,
-      template: './src/index.html',
+      template: './src/pages/index.html',
       filename: 'index.html',
+      chunks: ['main'],
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/pages/articles.html',
+      filename: 'articles.html',
+      chunks: ['articles'],
     }),
     new WebpackMd5Hash(),
   ],
