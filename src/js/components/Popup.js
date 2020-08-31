233b061ded640d657e79500677a3d7f840a9cc64
Aleksei Smolyaninov <salexxx@ngs.ru>
2020-08-31 17:46:25 +0700

    popup работает, переключается но привязывает оба слушателя на одну кнопку

--- src/js/components/Popup.js
+++ src/js/components/Popup.js
@@ -1,19 +1,26 @@
 export default class Popup {
   constructor(contentId) {
-    this.elem = document.querySelector(`#${contentId}`);
-  }
-
-  open() {
-    this.elem.style = '';
+    this.content = document.querySelector(`#${contentId}`);
+    this.popup = document.querySelector('.popup');
+    this.closebtn = document.querySelector('.popup__close');
+    this.closebtn.addEventListener('click', this.close.bind(this));
   }
 
   close() {
-    this.elem.style = 'display: none';
+    this._clearContent();
+    this.popup.classList.remove('popup_is-opened');
   }
-  setContent() {
 
+  open() {
+    this.popup.classList.add('popup_is-opened');
+    this._setContent();
   }
-  clearContent() {
 
+  _setContent() {
+    this.content.classList.add('popup__content_is-opened');
+  }
+
+  _clearContent() {
+    this.content.classList.remove('popup__content_is-opened');
   }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -8,17 +8,40 @@ import Popup from './components/Popup';
 let cardsArray = [];
 const cardlist = new ResultList(document.querySelector('.result__articles'), cardsArray);
 
-const mobilemenu = document.querySelector('.popupmenu__icon');
+/* const mobilemenu = document.querySelector('.popupmenu__icon');
 const popupMenu = new Popup('mobilemenu');
-mobilemenu.addEventListener('click', () => {popupMenu.open();});
+mobilemenu.addEventListener('click', () => {
+  popupMenu.toggle();
+  popupMenu.setContent();
+}); */
 
 const authoriz = document.querySelector('.header__nav_button');
 const popupAuthoriz = new Popup('authoriz');
+const popupAuthen = new Popup('authen');
+const toggletoAuthen = document.querySelector('.toggle-to-authen');
+const toggletoAuthoriz = document.querySelector('.toggle-to-authoriz');
+// const close = document.querySelector('.popup__close');
+
+/* close.addEventListener('click', () => {
+  popupAuthen.close();
+}); */
+
+toggletoAuthen.onclick = () => {
+  popupAuthoriz.close();
+  popupAuthen.open();
+};
+toggletoAuthoriz.onclick = () => {
+  popupAuthen.close();
+  popupAuthoriz.open();
+};
+
 authoriz.addEventListener('click', () => {
   popupAuthoriz.open();
-
 });
 
+
+
+ 
 
 document.forms.searchForm.onsubmit = (e) => {
   e.preventDefault();
