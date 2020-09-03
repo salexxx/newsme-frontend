3c5b415e5aa8ee1f428b134cb3ab6d2e3febda30
Aleksei Smolyaninov <salexxx@ngs.ru>
2020-09-03 17:48:48 +0700

    добавил очистку форм

--- src/js/components/Popup.js
+++ src/js/components/Popup.js
@@ -1,12 +1,14 @@
 export default class Popup {
   constructor(contentId) {
     this.content = document.querySelector(`#${contentId}`);
+    this.form = this.content.querySelector('.popup__form');
     this.popup = document.querySelector('.popup');
     this.closebtn = this.content.querySelector('.popup__close');
     this.closebtn.addEventListener('click', this.close.bind(this));
   }
 
   close() {
+    this.form.reset();
     this._clearContent();
     this.popup.classList.remove('popup_is-opened');
   }
