0fe766b711e04e721451bb885ec183196c4af3c1
Aleksei Smolyaninov <salexxx@ngs.ru>
2020-09-05 21:36:36 +0700

    отрисовка результатов по три карточки

--- src/js/components/ResultList.js
+++ src/js/components/ResultList.js
@@ -2,30 +2,46 @@ export default class ResultList {
   constructor(container, cardsArray) {
     this.container = container;
     this.cardsArray = cardsArray;
+    this.button = document.querySelector('.result__button');
+    this.button.addEventListener('click', () => this._renderThree());
   }
 
   addCard(cardElement) {
     this.container.appendChild(cardElement);
-    this.cardsArray.push(cardElement);
   }
 
   renderResults(cardsArray) {
+    this.cardsArray = cardsArray;
     if (cardsArray.length === 0) {
       this._renderNotFound();
       document.querySelector('.result__title').classList.add('result__title_invisible');
       document.querySelector('.result__articles').classList.add('result__articles_invisible');
       document.querySelector('.result__button').classList.add('result__button_invisible');
-      return; // document.querySelector('.result__zero').classList.add('invisible');
+      return;
     }
     document.querySelector('.result__title').classList.remove('result__title_invisible');
     document.querySelector('.result__articles').classList.remove('result__articles_invisible');
     document.querySelector('.result__button').classList.remove('result__button_invisible');
     document.querySelector('.result__zero').classList.add('invisible');
-    // this._renderNotFound();
     this._clearResults();
-    cardsArray.forEach((elem) => {
+
+    this._renderThree();
+  }
+
+  _renderThree() {
+    const arr = this.cardsArray.splice(0, 3);
+    arr.forEach((elem) => {
       this.addCard(elem);
     });
+    this._scroll();
+    if (this.cardsArray.length === 0) {
+      this.button.classList.add('result__button_invisible');
+    }
+  }
+
+  _scroll() {
+    this.button = document.querySelector('.result__button');
+    this.button.scrollIntoView({ behavior: 'smooth', block: 'end' });
   }
 
   _clearResults() {
